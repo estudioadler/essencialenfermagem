43f872589b735a2abb002a699ab2be51855d2ea8
estudioadler <adler.gabriel20@gmail.com>
2024-11-24 16:51:26 -0300

    fix: typing errors when uploading files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import { Benefits } from "@/components/Benfits/Benefits";
-import Contact from "@/components/Contact/Contact";
 import { DiferencialAccordion } from "@/components/DiferencialAccordion/DiferencialAccordion";
 import { Footer } from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
 import { Hero } from "@/components/Hero/Hero";
-import Preloader from "@/components/Preloader/Preloader";
 import { SectionAbout } from "@/components/SectionAbout/SectionAbout";
 import Testmonial from "@/components/Testmonial/Testmonial";
 import WorkContact from "@/components/WorkContact/WorkContact";
